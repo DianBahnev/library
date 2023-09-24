6a6a40e046ade38cabfbd26a3594cd6a2cffd827
Dian Bhanev <homeEmail@learning.com>
2023-09-24 19:06:58 +0100

    Adding initial code from SpringBoot initializer

---
+++ src/main/java/com/learning/libraryManagerSpringBoot/LibraryManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.learning.libraryManagerSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/libraryManagerSpringBoot/LibraryManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.libraryManagerSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
